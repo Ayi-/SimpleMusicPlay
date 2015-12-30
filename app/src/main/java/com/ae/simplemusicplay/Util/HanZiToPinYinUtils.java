a239bf50817d10b46c50f2869c252a1e658b5c75
LinHrU <coderboyone@Gmail.com>
2015-12-30 15:22:20 +0800

    第一次

---
+++ app/src/main/java/com/ae/simplemusicplay/Util/HanZiToPinYinUtils.java
@@ -0,0 +1,17 @@
+package com.ae.simplemusicplay.Util;
+
+/**
+ * Created by LinHrU on 2015/12/30.
+ *
+ */
+public class HanZiToPinYinUtils {
+
+    /**把中文转化成拼音
+     *
+     * @param s
+     * @return
+     */
+    public static String HanZiToPinYin(String s){
+
+    }
+}
