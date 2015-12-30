f40eb76911d24a7ddb7566f87d3234d8975b0e62
LinHrU <coderboyone@Gmail.com>
2015-12-30 15:31:51 +0800

    第一次完成汉字转拼音

--- app/src/main/java/com/ae/simplemusicplay/Util/HanZiToPinYinUtils.java
+++ app/src/main/java/com/ae/simplemusicplay/Util/HanZiToPinYinUtils.java
@@ -1,5 +1,10 @@
 package com.ae.simplemusicplay.Util;
 
+import net.sourceforge.pinyin4j.PinyinHelper;
+import net.sourceforge.pinyin4j.format.HanyuPinyinOutputFormat;
+import net.sourceforge.pinyin4j.format.HanyuPinyinToneType;
+import net.sourceforge.pinyin4j.format.exception.BadHanyuPinyinOutputFormatCombination;
+
 /**
  * Created by LinHrU on 2015/12/30.
  *
@@ -12,6 +17,23 @@ public class HanZiToPinYinUtils {
      * @return
      */
     public static String HanZiToPinYin(String s){
+        HanyuPinyinOutputFormat format = new HanyuPinyinOutputFormat();
+        format.setToneType(HanyuPinyinToneType.WITHOUT_TONE);
 
+        char[] input = s.trim().toCharArray();
+        String output = "";
+
+        try{
+            for (int i = 0; i <input.length ; i++) {
+                if (java.lang.Character.toString(input[i]).matches("[\\u4E00-\\u9FA5]+")){
+                    String[] temp = PinyinHelper.toHanyuPinyinStringArray(input[i], format);
+                    output += temp[0];
+                }else
+                    output += java.lang.Character.toString(input[i]);
+            }
+        } catch (BadHanyuPinyinOutputFormatCombination e) {
+            e.printStackTrace();
+        }
+        return output;
     }
 }
